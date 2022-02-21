d02d7811101227c51698b13a38c326db2c41eeaa
nipipa <93047919+nipipa@users.noreply.github.com>
2022-02-21 21:55:42 +0300

    Update main.c

--- main.c
+++ main.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
 int main() {
-  int i, j, o = 0;
-  int a, b, d = 0;
+  int i = 0, j = 0, o = 0;
+  int a = 0, b = 0, d = 0;
   int mass_1[100] = {0};
   int mass_2[100] = {0};
-
-  printf("Введите количетство элементов массива (макс 100)\n");
+  printf("Введите количетство элементов массива (макс 100):\n");
   scanf("%d", &j);
   if (j > 100) {
     j = 100;
   }
   for (i = 0; i < j; i++) {
-    printf("%d член = ", i);
+    printf("%d член => ", i);
     scanf("%d", &mass_1[i]);
+  }
+  printf("Ваш массив из десятичных чисел:\n");
+  for (i = 0; i < j; i++) {
+    printf("%d член => ", i);
+    printf("%d\n", mass_1[i]);
   }
   for (i = 0; i < j; i++) {
     a = mass_1[i];
+    b, d = 0;
     while (a != 0) {
       b = b * 10 + a % 2;
       a = a / 2;
@@ -27,14 +32,9 @@ int main() {
     }
     mass_2[i] = d;
   }
-  printf("Ваш массив из десятичных чисел:\n");
-  for (i = 0; i < j; i++) {
-    printf("%d член = ", i);
-    printf("%d\n", mass_1[i]);
-  }
   printf("\nВаш массив из двоичных чисел:\n");
   for (i = 0; i < j; i++) {
-    printf("%d член = ", i);
+    printf("%d член => ", i);
     printf("%d\n", mass_2[i]);
   }
   return 0;
