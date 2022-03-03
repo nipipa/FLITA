40412afb9dfe5257f72f20a0970a771624f0a0e7
nipipa <93047919+nipipa@users.noreply.github.com>
2022-03-03 08:04:49 +0300

    Update homework_1_var2.c

--- homework_1_var2.c
+++ homework_1_var2.c
@@ -1,13 +1,18 @@
 #include <stdio.h>
 #include <math.h>
 
-int DecToBin(int x) // функция перевода из 10 в 2 СИ
+int DecToBin(int x, int y) // функция перевода из 10 в 2 СИ
 {
     int i, b,c=0;
     for (i = 0; x > 0; i++){
         b = x % 2;
         x = (x - b) / 2;
         c += b * pow(10, i);
+        if (y == 1){ // условие перевода двоичного числа в отрицитаельное
+        if (c == 1) c=0;
+        else c=1;
+        }
+        
     }
 }
 int main()
@@ -24,12 +29,20 @@ int main()
     for (i = 0; i < j; i++){
         printf("%d элемент => ", i); // задаем массив элементов, записанных пользователем в терминале
         scanf("%d", &mass_1[i]);
+        if (mass_1[i] < 0){
+            mass_2[i] = 1;
+            mass_1[i] = mass_1[i] * (-1);
+        }
+        else mass_2[i] = 0;
     }
     printf("----------------------\n");
     printf("Dec:\t<==>\tBin:\n");
     for (i = 0; i < j; i++){
+        int e;
+        if (mass_2[i] == 1) e = mass_1[i] * (-1); // добавляем переменную для корректного вывода результата
+        else e = mass_1[i];
         printf("[%d]", i);
-        printf("\t%d\t\t", mass_1[i]); // выводим наш массив, состоящих из чисел в десятичной системе
-        printf("%d\n", DecToBin(mass_1[i]));
+        printf("%d\t\t", e); // выводим наш массив, состоящих из чисел в десятичной системе
+        printf("%d\n", DecToBin(mass_1[i],mass_2[i]));
     }
 }
